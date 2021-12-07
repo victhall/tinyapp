46efb5c8ecb1326203610968cf6fb14ebd199dbd
“Victoria <victoriahall.dev@gmail.com>
2021-12-07 20:20:28 +0000

    Added delete route

--- express_server.js
+++ express_server.js
@@ -68,7 +68,13 @@ app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
-})
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+const shortURL = req.params.shortURL;
+delete urlDatabase[shortURL];
+res.redirect("/urls")
+});
 
 //Allows server to retreieve or "listen" for requests
 app.listen(PORT, () => {
