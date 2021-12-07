4f7385e0896505303e25da7f1f64fb189545afde
“Victoria <victoriahall.dev@gmail.com>
2021-12-07 19:11:37 +0000

    Enabled shortURL to redirect to longURL and random string generator added

--- express_server.js
+++ express_server.js
@@ -11,6 +11,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Random string generator
+const generateRandomString = function() {
+    let newStr = Math.random().toString(36).slice(2, 8);
+    return newStr;
+  };
+
 //express.get takes in 2 params (request, response)
 //When client is connected, they receieve "hello" on their end.
 app.get("/", (req, res) => {
@@ -22,6 +31,7 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -39,10 +49,26 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  const randomStr = generateRandomString();
+  urlDatabase[randomStr] = req.body.longURL;
+  res.redirect(`/urls/${randomStr}`)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+})
 
 //Allows server to retreieve or "listen" for requests
 app.listen(PORT, () => {
